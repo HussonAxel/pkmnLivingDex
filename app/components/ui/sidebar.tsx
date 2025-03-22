3a10bc39696451d6d08dbff7b7f1cc9a6cdd4ba5
axel husson <aaaxel.hsson@gmail.com>
2025-03-22 13:45:46 +0100

    Enhance SidebarLink styling: add 'capitalize' class for improved text presentation

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -228,7 +228,7 @@ export const SidebarLink = ({
                       : "inline-block",
                     opacity: animate ? (open ? 1 : 0) : 1,
                   }}
-                  className="text-neutral-700 dark:text-neutral-200 group-hover/sidebar:translate-x-1 transition duration-150 whitespace-pre inline-block !p-0 !m-0"
+                  className="text-neutral-700 dark:text-neutral-200 group-hover/sidebar:translate-x-1 transition duration-150 whitespace-pre inline-block !p-0 !m-0 capitalize"
                 >
                   {sublink.label}
                 </motion.span>
